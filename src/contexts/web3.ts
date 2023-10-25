a7ba085dc2599e0b07f24a90e01246255c615c80
Arthur Schoppenhauer <max.david1007@gmail.com>
2023-10-25 02:04:26 +0200

    add env + editorconfig

--- src/contexts/web3.ts
+++ src/contexts/web3.ts
@@ -1,203 +1,202 @@
 import { configureChains, createConfig, connect, disconnect, InjectedConnector, readContract, writeContract, waitForTransaction, multicall } from "@wagmi/core";
-import { publicProvider } from "@wagmi/core/providers/public";
+import { jsonRpcProvider } from "@wagmi/core/providers/jsonRpc";
+import { parseAbiItem, parseEther, type PublicClient, type FallbackTransport } from "viem";
 import { readable, writable } from "svelte/store";
-import { parseAbi, parseAbiItem, parseEther } from "viem";
-import { goerli } from "viem/chains";
+import { base } from "viem/chains";
 import { cachedStore, consistentStore } from "../helpers/reactivity-helpers";
 import { BATCH_COST } from "../values";
 
 export function createWeb3Ctx() {
-    const { publicClient: clientDef, webSocketPublicClient } = configureChains(
-        [goerli],
-        [publicProvider()]
-    );
+	const PXLS = import.meta.env.VITE_PXLS
+	const PLTS = import.meta.env.VITE_PLTS
+	const USDC = import.meta.env.VITE_USDC
 
-    // PXLS
-    // PLTS
-    const PXLS = "0xC35632A3c400DB0bC1ee1855cdcec3c0008AD7E3"
-    const PLTS = "0x9BD837e9e87114DA3FA682f805ecB8b8D6CFeae4"
-    const USD = "0xB714d4Fd7f3B1af0D21587BD00f1667299553D41" // 0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc
+	let publicClient: PublicClient<FallbackTransport>
 
-    const { publicClient } = createConfig({
-        autoConnect: true,
-        publicClient: clientDef,
-        webSocketPublicClient,
-    });
+	const ctx = {
+		account: cachedStore(writable<`0x${string}` | null>()),
 
-    const ctx = {
-        account: cachedStore(writable<`0x${string}` | null>()),
+		async connect() {
+			const { chains, publicClient: publicClientGetter } = configureChains(
+				[base],
+				[jsonRpcProvider({ rpc: (_) => ({ http: import.meta.env.VITE_RPC_URL }), })],
+			)
 
-        async connect() {
-            const { account } = await connect({
-                connector: new InjectedConnector(),
-            });
-            console.log(account)
-            ctx.account.set(account)
-        },
+			const { publicClient: pc, connectors } = createConfig({
+				autoConnect: true,
+				connectors: [new InjectedConnector({ chains })],
+				publicClient: publicClientGetter
+			})
+			publicClient = pc
 
-        async disconnect() {
-            await disconnect()
-            ctx.account.set(null)
-        },
+			const { account } = await connect({ connector: connectors[0]! });
+			console.log(account)
+			ctx.account.set(account)
+		},
 
-        usdPrice: consistentStore(readable<number>(1700, (set) => {
-            readContract({
-                address: USD,
-                abi: [parseAbiItem("function getReserves() public view returns (uint112 _reserve0, uint112 _reserve1, uint32 _blockTimestampLast)")],
-                functionName: "getReserves"
-            }).then(r => {
-                const price = (r[0] * (10n ** (18n-6n))) / r[1];
-                console.log(price);
-                set(Number(price));
-            });
-        })),
+		async disconnect() {
+			await disconnect()
+			ctx.account.set(null)
+		},
 
-        pixels: consistentStore(writable<number[][]>([], (set) => {
-            ctx.account.subscribe(async acc => {
-                if (!acc) {
-                    set([])
-                    return
-                }
-                
-                const pixels = (await readContract({
-                    address: PXLS,
-                    abi: [parseAbiItem("function pixelsOf(address addr) external view returns (uint8[][] memory)")],
-                    functionName: "pixelsOf",
-                    args: [acc],
-                })) as number[][];
-                
-                set(pixels)
-            })
-        })),
+		usdPrice: consistentStore(readable<number>(1700, (set) => {
+			readContract({
+				address: USDC,
+				abi: [parseAbiItem("function getReserves() public view returns (uint112 _reserve0, uint112 _reserve1, uint32 _blockTimestampLast)")],
+				functionName: "getReserves"
+			}).then(r => {
+				const price = (r[0] * (10n ** (18n-6n))) / r[1];
+				console.log(price);
+				set(Number(price));
+			});
+		})),
 
-        plates: consistentStore(writable<{ pixels: number[][], delays: number[][] }[]>([], (set) => {
-            ctx.account.subscribe(async acc => {
-                if (!acc) {
-                    set([])
-                    return
-                }
+		pixels: consistentStore(writable<number[][]>([], (set) => {
+			ctx.account.subscribe(async acc => {
+				if (!acc) {
+					set([])
+					return
+				}
+				
+				const pixels = (await readContract({
+					address: PXLS,
+					abi: [parseAbiItem("function pixelsOf(address addr) external view returns (uint8[][] memory)")],
+					functionName: "pixelsOf",
+					args: [acc],
+				})) as number[][];
+				
+				set(pixels)
+			})
+		})),
 
-                const numNfts = await readContract({
-                    address: PLTS,
-                    abi: [parseAbiItem("function balanceOf(address owner) view returns (uint256)")],
-                    functionName: "balanceOf",
-                    args: [acc],
-                });
+		plates: consistentStore(writable<{ pixels: number[][], delays: number[][] }[]>([], (set) => {
+			ctx.account.subscribe(async acc => {
+				if (!acc) {
+					set([])
+					return
+				}
 
-                const tokenIds = (await multicall({
-                    contracts: Array(Number(numNfts)).fill(1).map(i => ({
-                        address: PLTS,
-                        abi: [parseAbiItem("function tokenOfOwnerByIndex(address owner, uint256 idx) view returns (uint256)")],
-                        functionName: "tokenOfOwnerByIndex",
-                        args: [acc, BigInt(i)],
-                    })),
-                })).filter(d => d.result).map(d => d.result!)
+				const numNfts = await readContract({
+					address: PLTS,
+					abi: [parseAbiItem("function balanceOf(address owner) view returns (uint256)")],
+					functionName: "balanceOf",
+					args: [acc],
+				});
 
-                const plates = (await multicall({
-                    contracts: tokenIds.map(i => ({
-                        address: PLTS,
-                        abi: [parseAbiItem("function underlyingPixels(uint256 id) view returns (uint8[][], (uint256, uint16)[])")],
-                        functionName: "underlyingPixels",
-                        args: [i],
-                    })),
-                })).filter(d => d.result).map(d => d.result as unknown as [number[][], number[][]]).map(d => ({ pixels: d[0], delays: d[1] }))
+				const tokenIds = (await multicall({
+					contracts: Array(Number(numNfts)).fill(1).map(i => ({
+						address: PLTS,
+						abi: [parseAbiItem("function tokenOfOwnerByIndex(address owner, uint256 idx) view returns (uint256)")],
+						functionName: "tokenOfOwnerByIndex",
+						args: [acc, BigInt(i)],
+					})),
+				})).filter(d => d.result).map(d => d.result!)
 
-                set(plates)
-            })
-        })),
+				const plates = (await multicall({
+					contracts: tokenIds.map(i => ({
+						address: PLTS,
+						abi: [parseAbiItem("function underlyingPixels(uint256 id) view returns (uint8[][], (uint256, uint16)[])")],
+						functionName: "underlyingPixels",
+						args: [i],
+					})),
+				})).filter(d => d.result).map(d => d.result as unknown as [number[][], number[][]]).map(d => ({ pixels: d[0], delays: d[1] }))
 
-        async conjure(batches: number) {
-            const { hash } = await writeContract({
-                address: PXLS,
-                abi: [parseAbiItem('function conjure(uint256 batches) external payable')],
-                functionName: 'conjure',
-                args: [BigInt(batches)],
-                value: parseEther(BATCH_COST.toString()) * BigInt(batches)
-            })
+				set(plates)
+			})
+		})),
 
-            const { status, blockNumber } = await waitForTransaction({ hash })
-            if (status == "reverted") throw "reverted"
+		async conjure(batches: number) {
+			const { hash } = await writeContract({
+				address: PXLS,
+				abi: [parseAbiItem('function conjure(uint256 batches) external payable')],
+				functionName: 'conjure',
+				args: [BigInt(batches)],
+				value: parseEther(BATCH_COST.toString()) * BigInt(batches)
+			})
 
-            const ethFoundLogs = await publicClient.getLogs({
-                address: PXLS,
-                event: parseAbiItem('event EthFound(address indexed to, uint256 amount)'),
-                args: {
-                    to: ctx.account.current
-                },
-                fromBlock: blockNumber,
-                toBlock: blockNumber
-            })
+			const { status, blockNumber } = await waitForTransaction({ hash })
+			if (status == "reverted") throw "reverted"
 
-            const ethFound = ethFoundLogs[0]?.args.amount ?? BigInt(0)
+			const ethFoundLogs = await publicClient.getLogs({
+				address: PXLS,
+				event: parseAbiItem('event EthFound(address indexed to, uint256 amount)'),
+				args: {
+					to: ctx.account.current
+				},
+				fromBlock: blockNumber,
+				toBlock: blockNumber
+			})
 
-            const conjuredLogs = await publicClient.getLogs({
-                address: PXLS,
-                event: parseAbiItem('event Conjured(address indexed to, uint8[][] pixels)'),
-                args: {
-                    to: ctx.account.current
-                },
-                fromBlock: blockNumber,
-                toBlock: blockNumber
-            })
+			const ethFound = ethFoundLogs[0]?.args.amount ?? BigInt(0)
 
-            const pixels = conjuredLogs[0].args.pixels! as number[][]
-            ctx.pixels.update(c => c.concat(pixels))
+			const conjuredLogs = await publicClient.getLogs({
+				address: PXLS,
+				event: parseAbiItem('event Conjured(address indexed to, uint8[][] pixels)'),
+				args: {
+					to: ctx.account.current
+				},
+				fromBlock: blockNumber,
+				toBlock: blockNumber
+			})
 
-            return [pixels, ethFound] as const
-        },
+			const pixels = conjuredLogs[0].args.pixels! as number[][]
+			ctx.pixels.update(c => c.concat(pixels))
 
-        async mint(indices: number[], delays: number[][]) {
-            const { hash } = await writeContract({
-                address: PXLS,
-                abi: [parseAbiItem('function mint(uint256[] calldata indices, uint256[] calldata delays) external')],
-                functionName: 'mint',
-                args: [indices.map(i => BigInt(i)), delays.flat().map(i => BigInt(i))]
-            })
+			return [pixels, ethFound] as const
+		},
 
-            const { status, blockNumber } = await waitForTransaction({ hash })
-            if (status == "reverted") throw "reverted"
+		async mint(indices: number[], delays: number[][]) {
+			const { hash } = await writeContract({
+				address: PXLS,
+				abi: [parseAbiItem('function mint(uint256[] calldata indices, uint256[] calldata delays) external')],
+				functionName: 'mint',
+				args: [indices.map(i => BigInt(i)), delays.flat().map(i => BigInt(i))]
+			})
 
-            const mintedLogs = await publicClient.getLogs({
-                address: PLTS,
-                event: parseAbiItem('event Transfer(address indexed from, address indexed to, uint256 indexed tokenId)'),
-                args: {
-                    from: '0x0000000000000000000000000000000000000000',
-                    to: ctx.account.current
-                },
-                fromBlock: blockNumber,
-                toBlock: blockNumber
-            })
+			const { status, blockNumber } = await waitForTransaction({ hash })
+			if (status == "reverted") throw "reverted"
 
-            ctx.pixels.update(c => {
-                const plate: number[][] = []
-                
-                const m = [...c]
-                const indicesSorted = indices.sort((a, b) => b - a)
-                // pop used pixels
-                for (let i = 0; i < indicesSorted.length; i++) {
-                    const idx = indicesSorted[i];
-                    const len = m.length - 1;
-                    if (idx < len) {
-                        m[idx] = m[len];
-                        plate.push(m[idx])
-                    }
-                    m.pop();
-                }
+			const mintedLogs = await publicClient.getLogs({
+				address: PLTS,
+				event: parseAbiItem('event Transfer(address indexed from, address indexed to, uint256 indexed tokenId)'),
+				args: {
+					from: '0x0000000000000000000000000000000000000000',
+					to: ctx.account.current
+				},
+				fromBlock: blockNumber,
+				toBlock: blockNumber
+			})
 
-                ctx.plates.update(c => c.concat([{ pixels: plate, delays }]))
+			ctx.pixels.update(c => {
+				const plate: number[][] = []
+				
+				const m = [...c]
+				const indicesSorted = indices.sort((a, b) => b - a)
+				// pop used pixels
+				for (let i = 0; i < indicesSorted.length; i++) {
+					const idx = indicesSorted[i];
+					const len = m.length - 1;
+					if (idx < len) {
+						m[idx] = m[len];
+						plate.push(m[idx])
+					}
+					m.pop();
+				}
 
-                return m
-            })
+				ctx.plates.update(c => c.concat([{ pixels: plate, delays }]))
 
-            return mintedLogs[0].args.tokenId!
-        },
+				return m
+			})
 
-        async burn() {
-            // TODO 
-        }
-    }
+			return mintedLogs[0].args.tokenId!
+		},
 
-    ctx.connect().then(() => ctx.pixels.subscribe(() => {})()) // connect and force sub pixels
+		async burn() {
+			// TODO
+		}
+	}
 
-    return ctx
+	ctx.connect().then(() => ctx.pixels.subscribe(() => {})()) // connect and force sub pixels
+
+	return ctx
 }
