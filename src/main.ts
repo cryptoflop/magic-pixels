07769dae0ea27bf9ac03e5463b3428eb459ab7ce
Arthur Schoppenhauer <max.david1007@gmail.com>
2023-11-15 03:00:41 +0100

    add cursor data uris

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import './main.css'
 import App from './App.svelte'
 
+import cursor from './assets/images/cursor.png';
+import pointCursor from './assets/images/cursor-point.png';
+import grabCursor from './assets/images/cursor-grab.png';
+import grabbingCursor from './assets/images/cursor-grabbing.png';
+
+document.documentElement.style.setProperty('--cursor-default', `url(${cursor}), auto`);
+document.documentElement.style.setProperty('--cursor-point', `url(${pointCursor}), auto`);
+document.documentElement.style.setProperty('--cursor-grab', `url(${grabCursor}), auto`);
+document.documentElement.style.setProperty('--cursor-grabbing', `url(${grabbingCursor}), auto`);
+
 const app = new App({
 	target: document.getElementById('app')!,
 })
