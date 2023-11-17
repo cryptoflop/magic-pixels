c202ae9f5bf47fa24e3c0b2a56bd39efcd96da94
Arthur Schoppenhauer <max.david1007@gmail.com>
2023-11-17 22:19:22 +0100

    upgrade plates proxy

--- contracts/scripts/MagicPlates.ts
+++ contracts/scripts/MagicPlates.ts
@@ -4,13 +4,14 @@ export async function deployPlts () {
   // const [creatooor] = await ethers.getSigners();
 
 	const MagicPlates = await ethers.getContractFactory('MagicPlates');
-	const plts = await upgrades.deployProxy(MagicPlates);
-	await plts.waitForDeployment()
+	// const plts = await upgrades.deployProxy(MagicPlates, { verifySourceCode: true });
+	// await plts.waitForDeployment()
 	
 	// upgrade
 	// const MagicPlatesV2 = await ethers.getContractFactory("MagicPlatesV2");
-  // const pltsv2 = await upgrades.upgradeProxy(PLTS_ADDRESS, MagicPlatesV2);
+  const plts = await upgrades.upgradeProxy("0x192E81ab95900dcEb05Efd646a8f224af34BDF9B", MagicPlates);
   // console.log("MagicPlates upgraded");
+	await plts.waitForDeployment()
 
 	return (await plts.getAddress()) as `0x${string}`
 }
