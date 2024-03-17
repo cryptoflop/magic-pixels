a51d5e0954e671bc4fe3febf1355cf9e836347b3
Arthur Schoppenhauer <max.david1007@gmail.com>
2024-03-17 13:50:58 +0100

    add nebula to background

--- src/elements/Background.svelte
+++ src/elements/Background.svelte
@@ -3,18 +3,52 @@
 	import { rndBtwn } from "../helpers/utils";
 	import { pixelColor, rndColorIdx } from "../helpers/color-utils";
 
-	let el: HTMLDivElement;
+	import nebula from "../assets/images/nebula.png";
+
+	let nebulas: HTMLDivElement;
+	let pixels: HTMLDivElement;
+
+	onMount(() => {
+		const images = Array.from(nebulas.children) as HTMLDivElement[];
+
+		images.forEach((img) => {
+			let currX = 0;
+			let currY = 0;
+			const anim = () => {
+				const posX = rndBtwn(-16, 16);
+				const posY = rndBtwn(-6, 6) * (currY > 0 ? -1 : 1);
+				img.animate(
+					[
+						{ transform: `translate(${currX}px, ${currY}px)` },
+						{ transform: `translate(${posX}px, ${posY}px)` },
+					],
+					{
+						duration: rndBtwn(4000, 6000),
+						iterations: 1,
+						easing: "ease-in-out",
+					},
+				).onfinish = anim;
+				currX = posX;
+				currY = posY;
+			};
+			anim();
+		});
+
+		return () => {
+			images.forEach((el) => el.getAnimations().map((a) => a.cancel()));
+		};
+	});
 
 	onMount(() => {
 		function spawnPixel() {
 			const pel = document.createElement("div");
 			pel.className = "absolute pointer-events-none w-1 h-1";
 			pel.style.transform = "translate(0, 0)";
-			el.appendChild(pel);
+			pixels.appendChild(pel);
 
 			function rndPoint() {
-				const w = el.clientWidth;
-				const h = el.clientHeight;
+				const w = pixels.clientWidth;
+				const h = pixels.clientHeight;
 
 				const wr = Math.round(rndBtwn(0, w));
 				const hr = Math.round(rndBtwn(0, h));
@@ -33,10 +67,10 @@
 			}
 
 			function animate() {
-				if (!el) return;
+				if (!pixels) return;
 				move();
 				pel.animate(getColorStops(), {
-					duration: 3000,
+					duration: 2200,
 					delay: rndBtwn(0, 1400),
 				}).onfinish = animate;
 			}
@@ -44,18 +78,32 @@
 			animate();
 		}
 
-		const pixelDensity = Math.round((el.clientWidth * el.clientHeight) / 40000);
+		const pixelDensity = Math.round(
+			(pixels.clientWidth * pixels.clientHeight) / 40000,
+		);
 		for (let i = 0; i < pixelDensity; i++) spawnPixel();
 
 		return () => {
-			Array.from(el.children).forEach((pel) =>
-				pel.getAnimations().map((a) => a.cancel()),
+			Array.from(pixels.children).forEach((el) =>
+				el.getAnimations().map((a) => a.cancel()),
 			);
 		};
 	});
 </script>
 
-<div
-	class="select-none overflow-hidden absolute inset-0 z-[-1]"
-	bind:this={el}
-/>
+<div class="absolute inset-0 pointer-events-none z-[-1]">
+	<div bind:this={nebulas}>
+		<img
+			src={nebula}
+			class="absolute opacity-30 w-screen bottom-[60vh]"
+			style="image-rendering: pixelated;"
+		/>
+		<img
+			src={nebula}
+			class="absolute opacity-30 w-screen top-[60vh]"
+			style="image-rendering: pixelated;"
+		/>
+	</div>
+
+	<div class="overflow-hidden absolute inset-0" bind:this={pixels} />
+</div>
